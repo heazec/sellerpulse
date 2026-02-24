2116bd7ad9fa28acbf4b40326193c597e4b9ae9c
heazec <98624245+heazec@users.noreply.github.com>
2026-02-24 15:57:57 -0800

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SellerPulse from './SellerPulse'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SellerPulse />
+  </React.StrictMode>
+)
